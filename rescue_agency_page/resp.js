b8812ac3bf4853ae0c8f6b6b9a84c4edfb301dcc
Parul <parul.srivastava.2004@gmail.com>
2023-10-06 17:21:37 +0530

    created new page for rescue agency info

---
+++ rescue_agency_page/resp.js
@@ -0,0 +1,8 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.nav-list')
+
+burger.addEventListener('click', ()=>{
+    navlist.classList.toggle('vis-res')
+    navbar.classList.toggle('navh-res')
+})
